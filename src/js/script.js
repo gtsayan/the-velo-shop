c572e1599d0df62e5ac689b2551fe78a48e61608
gtsayan <48355689+gtsayan@users.noreply.github.com>
2020-01-21 13:38:51 +0300

    FINAL

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    console.log("test")
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        items: 2,
+        center: true,
+    })
+});
